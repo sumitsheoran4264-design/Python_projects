9d373fdd2f4af8e01eca166be4ea04fd978821e3
Sumit <https://github.com/sumitsheoran4264-design/100-Days-of-Python>
2026-06-22 16:04:34 +0530

    first Python project higher lower

---
+++ data.py
@@ -0,0 +1,182 @@
+data = [
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 650,
+        "description": "Footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 510,
+        "description": "Footballer and World Cup winner",
+        "country": "Argentina"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 420,
+        "description": "Singer, actress, and producer",
+        "country": "United States"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 395,
+        "description": "Actor and former professional wrestler",
+        "country": "United States"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 390,
+        "description": "Businesswoman and reality TV personality",
+        "country": "United States"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 375,
+        "description": "Singer and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 360,
+        "description": "Media personality and entrepreneur",
+        "country": "United States"
+    },
+    {
+        "name": "Beyonce",
+        "follower_count": 315,
+        "description": "Singer, songwriter, and performer",
+        "country": "United States"
+    },
+    {
+        "name": "Khaby Lame",
+        "follower_count": 165,
+        "description": "Social media creator known for comedy videos",
+        "country": "Italy"
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 270,
+        "description": "Cricketer and former captain of India",
+        "country": "India"
+    },
+    {
+        "name": "Shah Rukh Khan",
+        "follower_count": 50,
+        "description": "Actor known as the King of Bollywood",
+        "country": "India"
+    },
+    {
+        "name": "Priyanka Chopra",
+        "follower_count": 92,
+        "description": "Actress, producer, and former Miss World",
+        "country": "India"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 280,
+        "description": "Singer-songwriter known for global hit albums",
+        "country": "United States"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 295,
+        "description": "Singer and songwriter",
+        "country": "Canada"
+    },
+    {
+        "name": "Kendall Jenner",
+        "follower_count": 290,
+        "description": "Model and television personality",
+        "country": "United States"
+    },
+    {
+        "name": "Nicki Minaj",
+        "follower_count": 230,
+        "description": "Rapper and singer",
+        "country": "Trinidad and Tobago"
+    },
+    {
+        "name": "Miley Cyrus",
+        "follower_count": 215,
+        "description": "Singer and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Jennifer Lopez",
+        "follower_count": 250,
+        "description": "Singer, actress, and dancer",
+        "country": "United States"
+    },
+    {
+        "name": "Billie Eilish",
+        "follower_count": 120,
+        "description": "Singer and Grammy Award winner",
+        "country": "United States"
+    },
+    {
+        "name": "Tom Holland",
+        "follower_count": 65,
+        "description": "Actor famous for playing Spider-Man",
+        "country": "United Kingdom"
+    },
+    {
+        "name": "Zendaya",
+        "follower_count": 180,
+        "description": "Actress and singer",
+        "country": "United States"
+    },
+    {
+        "name": "Robert Downey Jr.",
+        "follower_count": 60,
+        "description": "Actor famous for Iron Man role",
+        "country": "United States"
+    },
+    {
+        "name": "Chris Hemsworth",
+        "follower_count": 60,
+        "description": "Actor famous for Thor role",
+        "country": "Australia"
+    },
+    {
+        "name": "Emma Watson",
+        "follower_count": 75,
+        "description": "Actress known for Harry Potter films",
+        "country": "United Kingdom"
+    },
+    {
+        "name": "MrBeast",
+        "follower_count": 70,
+        "description": "YouTuber known for challenges and philanthropy",
+        "country": "United States"
+    },
+    {
+        "name": "PewDiePie",
+        "follower_count": 21,
+        "description": "YouTuber and content creator",
+        "country": "Sweden"
+    },
+    {
+        "name": "Rihanna",
+        "follower_count": 150,
+        "description": "Singer and businesswoman",
+        "country": "Barbados"
+    },
+    {
+        "name": "Drake",
+        "follower_count": 145,
+        "description": "Rapper and singer",
+        "country": "Canada"
+    },
+    {
+        "name": "Eminem",
+        "follower_count": 50,
+        "description": "Rapper and songwriter",
+        "country": "United States"
+    },
+    {
+        "name": "Lady Gaga",
+        "follower_count": 60,
+        "description": "Singer and actress",
+        "country": "United States"
+    }
+]
---
+++ higher_lower_project.py
@@ -0,0 +1,50 @@
+import random
+from art import logo, vs
+from data import data
+def format_data(account):
+    account_name = account['name']
+    account_descr = account['description']
+    account_country = account['country']
+    return(f"{account_name}, {account_descr}, from {account_country}.")
+
+
+def is_guess_correct(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == 'a'
+    else:
+        return guess == 'b'
+    
+account_b = random.choice(data)
+
+score = 0
+game_on = True
+print(logo)
+
+while game_on:
+    
+
+    account_a = account_b
+    account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    print("Account A:",format_data(account_a))
+
+    print(vs)
+
+    print("Against B:",format_data(account_b))
+
+    choice = input("How has more follower? Type A or B: ").lower()
+    print(logo)
+
+    account_a_follower = account_a['follower_count']
+    account_b_follower = account_b['follower_count']
+
+
+    if (is_guess_correct(choice, account_a_follower, account_b_follower)):
+        score += 1
+        print(f"You are right, your current score is {score}")
+    else:
+        print(f"You guess wrong, your final score is: {score}")
+        game_on = False
+        
